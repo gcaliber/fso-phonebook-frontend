1f514acd3e0832028861683fdd4f24bdb707f9dc
Michael Green <mgreen1980@gmail.com>
2022-03-03 13:05:33 -0500

    cleanup

--- src/services/phonebook.js
+++ src/services/phonebook.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'http://localhost:3001/api/persons' // development only
 // const baseUrl = '/api/persons'
 
 const getAll = () => {
